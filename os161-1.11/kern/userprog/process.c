24a429cd879b9025dce91f8cd1f25c26c619b60c
Smashking117 <jsbs5563@gmail.com>
2018-05-03 12:04:18 -0400

    Update process.c

--- os161-1.11/kern/userprog/process.c
+++ os161-1.11/kern/userprog/process.c
@@ -1,5 +1,5 @@
 #include <types.h>
-#include <errno.h>
+#include <kern/errno.h>
 #include <lib.h>
 #include <process.h>
 #include <addrspace.h>
@@ -10,13 +10,14 @@ void process_bootstrap(void){
 	int pid_table[32] = {0}; //initialize pid table
 	char *name = "initial";
 	curthread -> process = process_create(name);
+}
 
 struct process * process_create(char *name){
 	struct process *ret = (struct process *)kmalloc(sizeof(struct process));
 	ret->name = name;
 	ret->space = as_create();
 	ret->pid = pid_allocate();
-	if(ret->pid == NULL){
+	if(ret->pid == -1){
 		return NULL;
 	}
 	return ret;
